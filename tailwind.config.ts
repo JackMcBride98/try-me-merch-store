d2d305423cd91d7eed796f8446d3b2204a314be7
Jack McBride <jack.mcbride@ghyston.com>
2023-08-16 14:07:21 +0100

    Adding more images, build upcoming gigs table

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xxs: "350px",
+        xs: "400px",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
