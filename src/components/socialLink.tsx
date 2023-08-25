784f03b43eab9c1bc5defba0605a3e467e49cbd7
Jack McBride <jack.mcbride@ghyston.com>
2023-08-25 15:15:25 +0100

    Finalise landing page, add background gradient animation

--- src/components/socialLink.tsx
+++ src/components/socialLink.tsx
@@ -14,7 +14,7 @@ export const SocialLink = ({
   className,
 }: SocialLinkProps) => (
   <a
-    className={`${className} relative h-12 w-12 bg-transparent opacity-90 xs:scale-125 sm:h-14 sm:w-14`}
+    className={`${className} relative h-12 w-12 bg-transparent opacity-90 xs:scale-125 sm:h-14 sm:w-14 md:h-20 md:w-20 lg:h-28 lg:w-28 xl:h-40 xl:w-40`}
     rel="noopener noreferrer"
     target="_blank"
     href={link}
